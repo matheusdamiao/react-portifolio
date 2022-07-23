fcc0fa5954d5b78c7e140abeaa7c02aa59b41799
Matheus Damião <matheus.damiaoliveira@gmail.com>
2022-07-22 21:24:01 -0300

    fixing again

--- src/components/Contato.js
+++ src/components/Contato.js
@@ -14,7 +14,7 @@ const Contato = () => {
 
 
   const handleSubmit = e => {
-    e.preventDefault();
+    
     fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },

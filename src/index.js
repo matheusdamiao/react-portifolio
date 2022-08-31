702bb6c4c33af73514f73446d0e5f0a1e458f2d3
Matheus Damião <matheus.damiaoliveira@gmail.com>
2022-08-31 11:35:41 -0300

    trying to fix router error

--- src/index.js
+++ src/index.js
@@ -1,16 +1,43 @@
 import React from 'react';
 // import ReactDOM from 'react-dom/client';
 import { render } from 'react-dom'
-// import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Menu from './components/Menu'
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import Main from './components/Main';
+import Sobre from './components/Sobre';
+import Contato from './components/Contato';
+import NotFound from './components/NotFound';
+import Projetos from './components/Projetos';
+import ProjetoCompleto from './components/ProjetoCompleto';
 const root = document.getElementById('root');
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 render(
   
-    <App />
+    <BrowserRouter>
+        <Menu />
+          <Routes>
+                
+                <Route index path="/" element={<Main />} />
+                <Route path="/projetos" element={<Projetos/>}/>
+                <Route path=":projeto" element={<ProjetoCompleto/> } /> 
+                <Route path="projetos/:projeto" element={<ProjetoCompleto />}/>
+                <Route path="/sobre" element={<Sobre />} />
+                <Route path="/contato" element={<Contato />} />
+                <Route path="*" element={<NotFound />} />
+            
+          </Routes>
+      </BrowserRouter>
+
+
+
+
+
+
+    // <App />
   ,
   root
 );
